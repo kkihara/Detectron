772c538c6a1e22e4669547384c0cb39b7ab96247
kenny <kkihara1@gmail.com>
2018-04-14 00:35:57 +0000

    invert neck

--- tools/infer_video.py
+++ tools/infer_video.py
@@ -238,8 +238,8 @@ def convert_pose_data(data):
                 data[2, coco_keyps.index('right_shoulder')],
                 data[2, coco_keyps.index('left_shoulder')])
             if sc_neck > KEYP_THRESH:
-                left_shoulder = data[:2, coco_keyps.index('left_shoulder')]
-                right_shoulder = data[:2, coco_keyps.index('right_shoulder')]
+                left_shoulder = data[:2, coco_keyps.index('left_shoulder')][::-1]
+                right_shoulder = data[:2, coco_keyps.index('right_shoulder')][::-1]
                 new_data[idx:idx+2] = (left_shoulder + right_shoulder) / 2.0
         else:
             if data[2, coco_keyps.index(keyp)] > KEYP_THRESH:
